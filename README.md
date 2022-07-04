5a9e047fd9bea1e93d9160f27bce1407f7644d2a
Dmytro <deepdows@gmail.com>
2022-07-04 12:58:20 +0300

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# URL shortener
+
+Backend part was built with FastAPI python framework.
+Frontend part was built with VueJS
--- database.py
+++ database.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-
-#SQLALCHEMY_DATABASE_URL = "postgresql://user:secret@todo-db:5432/db"
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://SA6eP4SydD:lTqeHUpUYd@remotemysql.com:3306/SA6eP4SydD"
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
